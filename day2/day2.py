476d8c1fff6564fc288e3b38c985eab31868a5f2
Fern <fernanlukban@gmail.com>
2019-12-08 00:42:23 -0800

    Initial commit

---
+++ day2/day2.py
@@ -0,0 +1,17 @@
+def calculate_fuel(mass: int) -> int:
+    return mass//3 - 2
+
+def calculate_magic_fuel(mass: int, total: int = 0) -> int:
+    fuel = calculate_fuel(mass)
+    if fuel <= 0:
+        return total
+    return calculate_magic_fuel(fuel, total + fuel)
+
+total = 0
+
+with open("input2.txt", 'r') as input_file:
+    for line in input_file:
+        mass = int(line.strip())
+        total += calculate_magic_fuel(mass)
+
+print(total)
